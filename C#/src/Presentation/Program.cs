22d0439ca96dfc39cd719bea7f7ec0994c016c13
Yamkela <yamkela.qhogwana@outlook.com>
2025-07-08 12:47:31 +0200

    Initial Project Setup

---
+++ C#/src/Presentation/Program.cs
@@ -0,0 +1,24 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => "Hello World")
+    .Produces(200, typeof(string));
+
+app.Run();
+
